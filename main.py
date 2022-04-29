0bb25c60e6d2e3eff2ea5b72f181c8f8a7d43112
Jaden <heng.21@ichat.sp.edu.sg>
2022-04-29 14:56:36 +0800

    Initial version to display text message

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    list1 = get_user_input()
+    print(calc_average_temperature(list1))
+    calc_min_max_temperature(list1)
 
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    listoflists = input()
+    x = listoflists.split(",")
+    print(type(x))
+    listoffloats1 = [float(a) for a in x]
+    print(listoffloats1)
+    return listoffloats1
+
+def calc_average_temperature(x):
+    avg = sum(x) / len(x)
+    return avg
+
+def calc_min_max_temperature(y):
+    minlist = min(y)
+    print(minlist)
+    maxlist = max(y)
+    print(maxlist)
+
+main()
