c4806572ecf0dd3e94088793487f99076bf30bc7
Jaden <heng.21@ichat.sp.edu.sg>
2022-04-29 15:14:23 +0800

    updated file

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     list1 = get_user_input()
-    print(calc_average_temperature(list1))
+    print("Average: ",calc_average_temperature(list1))
     calc_min_max_temperature(list1)
+    print("Median: ",calc_median_temperature(list1))
 
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
@@ -22,8 +23,18 @@ def calc_average_temperature(x):
 
 def calc_min_max_temperature(y):
     minlist = min(y)
-    print(minlist)
+    print("Min: " , minlist)
     maxlist = max(y)
-    print(maxlist)
+    print("Max: " , maxlist)
+
+def calc_median_temperature(z):
+    z_sorted = z.sort()
+    if len(z) % 2 != 0:
+        m = int((len(z) + 1) / 2 - 1)
+        return z[m]
+    else:
+        m1 = int(len(z)/2 - 1)
+        m2 = int(len(z)/2)
+        return (z[m1]+z[m2])/2
 
 main()
